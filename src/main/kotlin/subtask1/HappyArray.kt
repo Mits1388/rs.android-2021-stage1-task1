6542847ee5657bf53c7c40790d174eb82fd3ae85
Mits16 <mits1388@gmail.com>
2021-05-25 11:48:38 +0300

    init: push code
    init: push code

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,28 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var sadArray1 = intArrayOf()
+        sadArray1 = sadArray
+        var  x = 0
+        var happyArray = arrayListOf<Int>()
+
+        if(sadArray.isEmpty()) return sadArray
+
+        while (x < 4){
+            var a = sadArray1.size - 1
+            var sizeSad = sadArray1.size - 3
+            happyArray.add(sadArray1[0])
+            for (i in 0 .. sizeSad){
+                var sum = sadArray1[i]+sadArray1[i + 2]
+                if(sum > sadArray1[i + 1]){
+                    happyArray.add(sadArray1[i + 1])
+                }
+            }
+            happyArray.add(sadArray1[a])
+            sadArray1 = happyArray.toIntArray()
+            happyArray = arrayListOf()
+            x++
+        }
+        return sadArray1
     }
 }
--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,23 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        var arraySum = arrayListOf<Int>()
+
+        var i = 0
+        val s = bill.size
+
+        while (i < s){
+            if( k != i) arraySum.add(bill[i])
+            i++
+        }
+
+        val item = arraySum.sum()/2
+
+        if(item == b){
+            return "bon appetit"
+        }else{
+            val debt =  b - item
+            return "$debt"
+        }
     }
 }
